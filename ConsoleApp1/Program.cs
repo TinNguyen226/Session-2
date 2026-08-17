f1bf63b0e3101001854a8afb2b46a0a476601bf2
tinguyen226 <thantinqh@gmail.com>
2026-08-17 15:38:01 +0700

    bai 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Nhap so a:");
+        float a = Convert.ToSingle(Console.ReadLine());
+        Console.Write("Nhap so b:");
+        float b = Convert.ToSingle(Console.ReadLine());
+        Console.Write($"Tong hai so a va b: = {a + b}");
+    }
+}
